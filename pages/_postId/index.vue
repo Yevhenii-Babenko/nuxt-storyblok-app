0309f32bb06dab7546869f828e4f2221fc335c52
yababenko1 <ybabenko@jappware.com>
2021-11-02 18:22:59 +0200

    added ability for dispaying single post

--- pages/_postId/index.vue
+++ pages/_postId/index.vue
@@ -1,21 +1,26 @@
 <template>
-    <div>
-        <h2></h2>
-        <p></p>
-    </div>
+  <div>
+      <img :src="img" alt="poster">
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+  </div>
 </template>
 
 <script>
-
 export default {
-    asyncData(context) {
-        return context.app.$storyapi.get('cdn/stories/blog/posts/' + context.params.postId, {
-            version: 'draft'
-        })
-        .then(response => {
-            console.log(response.data)
-        })
-    }
-}
-
+  asyncData(context) {
+    return context.app.$storyapi
+      .get("cdn/stories/" + context.params.postId, {
+        version: "draft",
+      })
+      .then((response) => {
+        console.log("this is a post", response.data);
+        return {
+          title: response.data.story.content.title,
+          content: response.data.story.content.postBody,
+          img: response.data.story.content.imgArea
+        };
+      });
+  },
+};
 </script>
