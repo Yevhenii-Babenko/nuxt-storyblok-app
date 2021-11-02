dd9695abb89ed6798af1afb7093cd72d113d19a6
yababenko1 <ybabenko@jappware.com>
2021-11-02 21:19:11 +0200

    commit

--- pages/_postId/index.vue
+++ pages/_postId/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-      <img :src="img" alt="poster">
+  <div v-editable="blok">
+    <img :src="img" alt="poster" />
     <h2>{{ title }}</h2>
     <p>{{ content }}</p>
   </div>
@@ -16,11 +16,29 @@ export default {
       .then((response) => {
         console.log("this is a post", response.data);
         return {
+          blok: response.data.story.content,
           title: response.data.story.content.title,
           content: response.data.story.content.postBody,
-          img: response.data.story.content.imgArea
+          img: response.data.story.content.imgArea,
         };
       });
+  },
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge();
+
+      storyblokInstance.on("input", (event) => {
+        if (Object.is(event.story.id, this.story.id)) {
+          this.story.content = event.story.content;
+        }
+      });
+      storyblokInstance.on(["published", "change"], (event) => {
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        });
+      });
+    });
   },
 };
 </script>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section v-editable="blok">
     <h2>{{ title }}</h2>
     <p>{{ content }}</p>
   </section>
@@ -13,12 +13,30 @@ export default {
         version: "draft",
       })
       .then((response) => {
-          console.log(response.data)
+        console.log(response.data);
         return {
+          blok: response.data.story.content,
           title: response.data.story.content.title,
           content: response.data.story.content.content,
         };
       });
+  },
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge();
+
+      storyblokInstance.on("input", (event) => {
+        if (Object.is(event.story.id, this.story.id)) {
+          this.story.content = event.story.content;
+        }
+      });
+      storyblokInstance.on(["published", "change"], (event) => {
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        });
+      });
+    });
   },
 };
 </script>
